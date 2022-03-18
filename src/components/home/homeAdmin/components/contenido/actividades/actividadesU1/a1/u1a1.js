3b5099e32b0a1e966f54787c882ff3f4f89d9a99
SamuelPadillaT <samuelpadilla102398@gmail.com>
2022-03-18 08:36:59 -0500

    Ubidad 1 Actividad 1 no terminada 1

--- src/components/home/homeAdmin/components/contenido/actividades/actividadesU1/a1/u1a1.js
+++ src/components/home/homeAdmin/components/contenido/actividades/actividadesU1/a1/u1a1.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import {Div, Footer} from './u1a1Style'
 
 class U1a1 extends Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      activity : 1
+    };
+  }
+
+  componentDidMount() {
+  }
+
   render(){
     return(
-      <div>Unidad1 Actvidad 1</div>
+      <div>
+        <Div>Unidad1 Actvidad 1</Div>
+        <Footer>
+          <button onClick = {() =>this.setState({ activity: 1 })}>-1-</button>
+          <button onClick = {() =>this.setState({ activity: 2 })}>-2-</button>
+          <button onClick = {() =>this.setState({ activity: 3 })}>-3-</button>
+          <button onClick = {() =>this.setState({ activity: 4 })}>-4-</button>
+        </Footer>
+      </div>
     );
   }
 }
---
+++ src/components/home/homeAdmin/components/contenido/actividades/actividadesU1/a1/u1a1Style.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+
+`
+
+const Footer = styled.footer`
+& button{
+    font-family: cursive;
+    display: inline-block;
+    padding: 5px;
+    border: 2px solid #000;
+    border-radius: 3px;
+    margin: 3px
+}
+`
+
+export {Div, Footer}
